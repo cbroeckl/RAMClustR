1b8ca772d7b1bd73b9a1dd7b69d453aaaccffae1
Corey Broeckling <cbroeckl@users.noreply.github.com>
2014-09-25 16:08:31 -0600

    Update ramclustR.R

--- R/ramclustR.R
+++ R/ramclustR.R
@@ -338,7 +338,7 @@ ramclustR<- function(  xcmsObj=NULL,
       }
     }
     dimnames(RC$SpecAbund)[[2]]<-paste("C", 1:ncol(RC$SpecAbund), sep="")
-    dimnames(RC$SpecAbund)[[1]]<-xset@phenoData[,1]	
+    dimnames(RC$SpecAbund)[[1]]<-xcmsObj@phenoData[,1]	
     g<-Sys.time()
     cat('\n', '\n')
     cat(paste("RAMClustR has collapsed feature quantities

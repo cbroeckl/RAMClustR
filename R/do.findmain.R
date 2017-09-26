932c04a149209059d3a4fee7f019bf551882cd7a
Corey Broeckling <cbroeckl@colostate.edu>
2017-09-26 09:47:07 -0600

    documentation

--- R/do.findmain.R
+++ R/do.findmain.R
@@ -9,15 +9,26 @@
 #' @param mode character: "positive" or "negative"
 #' @param mzabs.error numeric: absolute mass deviation allowd
 #' @param ppm.error numeric: ppm mass error added to mzabs.error
-#' @param ads character: vector of allowed adducts, i.e. c("[M+H]+")
-#' @param nls  character: vector of allowed neutral losses, i.e. c("[M+H-H2O]+")
+#' @param ads character: vector of allowed adducts, i.e. c("[M+H]+"). if NULL, default values of H+, Na+, K+, and NH4+, as monomer, dimer, and trimer, are assigned.
+#' @param nls  character: vector of allowed neutral losses, i.e. c("[M+H-H2O]+").  if NULL, an extensive list derived from CAMERA's will be used. 
 #' @param adwts numeric: vector of weights for adducts. length should match that of 'ads', else first value will be repeated.  if NULL (default), value of 1 is assigned to all. Only used for ramclustR scoring of M.
 #' @param nlwts numeric: vector of weights for neutral losses. length should match that of 'nls', else first value will be repeated.  if NULL (default), value of 0.1 is assigned to all.  Only used for ramclustR scoring of M.
 #' @param plot.findmain logical: should pdf polts be generated for evaluation?
 #' @param writeMat logical: should indidual .mat files (for MSFinder) be generated in a 'mat' subdirectory in the 'spectra' folder?
 
 #' @return a partially annotated ramclustR object.  base structure is that of a standard R heirarchical clustering output, with additional slots described in ramclustR documentation (?ramclustR).  New slots added after using the interpretMSSpectrum functionality include:
-#' @return more slots to describe.  
+#' @return more slots to describe. 
+#' @return M The inferred molecular weight of the compound giving rise to the each spectrum
+#' @return M.ppm The ppm error of all the MS signals annotated, high error values should be considered 'red flags'
+#' @return M.ann The annotated spectrum supporting the intepretation of M
+#' @return use.findmain  Logical vector indicating whether findmain scoring (TRUE) or ramclustR scoring (FALSE) was used to support inference of M.  By default, findmain scoring is used.  When ramclustR scoring differs from findmain scoring, the scoring metric which predicts higher M is selected. 
+#' @return M.ramclustr  M selected using ramclustR scoring
+#' @return M.ppm.ramclustr ppm error of M selected using ramclustR scoring
+#' @return M.ann.ramclustr annotated spectrum supporing M using ramclustR scoring
+#' @return M.findmain M selected using findmain scoring
+#' @return M.ppm.findmain ppm error of M selected using findmain scoring
+#' @return M.ann.findmain annotated spectrum supporing M using findmain scoring
+#' 
 #' @author Corey Broeckling
 #' @export
 
@@ -34,6 +45,10 @@ do.findmain<-function(ramclustObj = RC,
                       plot.findmain=TRUE, 
                       writeMat=TRUE) {
   require(InterpretMSSpectrum)
+  
+  #############
+  ## if adducts and neutral losses and weights are undefined, set to defaults
+  
   if(is.null(ads)) {
     ads<-c("[M+H]+", "[M+Na]+", "[M+K]+", "[M+NH4]+", "[2M+H]+", "[2M+Na]+", 
            "[2M+K]+", "[2M+NH4]+", "[3M+H]+", "[3M+Na]+", "[3M+K]+", "[3M+NH4]+")
@@ -94,9 +109,9 @@ do.findmain<-function(ramclustObj = RC,
   
   adnlwts<-c(adwts, nlwts); names(adnlwts)<-c(ads, nls)
   
-  # cat(names(adnlwts),'\n')
-  # cat(adnlwts,'\n')
-
+  #############
+  ##  create structure in ramclustObj for holding results
+  
   M.findmain <- rep(NA, max(ramclustObj$featclus))
   M.ppm.findmain <- rep(NA, max(ramclustObj$featclus))
   M.ann.findmain <- as.list(rep(NA, max(ramclustObj$featclus)))
@@ -105,8 +120,13 @@ do.findmain<-function(ramclustObj = RC,
   M.rank.ramclustr <- rep(NA, max(ramclustObj$featclus))
   M.ann.ramclustr <- as.list(rep(NA, max(ramclustObj$featclus)))
   
+  #############
+  ## define (if null) full compound range for analysis
   
   if(is.null(cmpd)) {cmpd <- (1:max(ramclustObj$featclus)) }
+  
+  #############
+  ## do findMain for all compounds in 'cmpd', append results to ramclustObj
   
   for(cl in cmpd){
     s<-data.frame("mz"=ramclustObj$fmz[which(ramclustObj$featclus==cl)], "int"=ramclustObj$msint[which(ramclustObj$featclus==cl)])
@@ -128,6 +148,10 @@ do.findmain<-function(ramclustObj = RC,
     M.findmain[cl]<-summarytable[1, "neutral_mass"]
     M.ppm.findmain[cl]<-summarytable[1, "medppm"]
     M.ann.findmain[[cl]]<-out[[1]]
+    
+    
+    #############
+    ## calculate ramclustR scoring
     
     ## set all major adduct ppm errors to half ppm.error, or 5
     for(y in 1:length(out)) {
@@ -152,10 +176,7 @@ do.findmain<-function(ramclustObj = RC,
         # data.frame(keep, wt, int, mzerr, 'ppm'=out[[x]][keep, "ppm"], massorder, 'score'=(massorder *int * mzerr * wt))
       }
     )
-    
-    ## the result with the best summaryscore can be selected 
-    ## and tagged as such somehow
-    
+
     best<-which.max(summaryscores)
     
     M.ramclustr[cl]<-summarytable[best, "neutral_mass"]
@@ -163,11 +184,15 @@ do.findmain<-function(ramclustObj = RC,
     M.rank.ramclustr[cl]<-best
     M.ann.ramclustr[[cl]]<-out[[best]]
     
+    #############
+    ## progress reporting
     if(10*round(cl/10, digits=0) == cl) {
       cat(cl, "of", max(ramclustObj$featclus), '\n')
     }
   } 
   
+  #############
+  ## append all results to ramclustObj
   ramclustObj$M.ramclustr <- M.ramclustr
   ramclustObj$M.ppm.ramclustr <- M.ppm.ramclustr
   ramclustObj$M.rank.ramclustr <- M.rank.ramclustr
@@ -177,6 +202,8 @@ do.findmain<-function(ramclustObj = RC,
   ramclustObj$M.ann.findmain <- M.ann.findmain
   ramclustObj$use.findmain <- rep(TRUE, length(M.ppm.findmain))
   
+  #############
+  ## determine inconsistencies between two scoring methods, select higher MW
   resolve <- which(abs(ramclustObj$M.ramclustr - ramclustObj$M.findmain)  > (2*mzabs.error))
   for(i in resolve) {
     if(ramclustObj$M.ramclustr[i] >  ramclustObj$M.findmain[i]) {
@@ -184,14 +211,16 @@ do.findmain<-function(ramclustObj = RC,
     }
   }
   
-  ramclustObj$M<-ramclustObj$M.findmain
-  ramclustObj$M[!ramclustObj$use.findmain]<-ramclustObj$M.ramclustr[!ramclustObj$use.findmain]
+  ramclustObj$M <- ramclustObj$M.findmain
+  ramclustObj$M[!ramclustObj$use.findmain] <- ramclustObj$M.ramclustr[!ramclustObj$use.findmain]
   ramclustObj$M.ann <- ramclustObj$M.ann.findmain
   resolved <- which(!ramclustObj$use.findmain)
   for(i in resolved) {
-    ramclustObj$M.ann[[i]]<-ramclustObj$M.ann.ramclustr[[i]]
+    ramclustObj$M.ann[[i]] <- ramclustObj$M.ann.ramclustr[[i]]
   }
-    
+  
+  #############
+  ## (optionally) plot spectra to pdf
   if(plot.findmain) {
     cat("plotting findmain annotation results", '\n')
     pdf("spectra/findmainPlots.pdf", width=10, height = 4.6)
@@ -209,41 +238,70 @@ do.findmain<-function(ramclustObj = RC,
         "M.findmain =", 
         round(ramclustObj$M.findmain[cl], digits=4), "( +/-", round(ramclustObj$M.ppm.findmain[cl], digits=1), 
         "ppm )"  ),
-            font = if(ramclustObj$use.findmain[cl]) {2} else {1}, col = if(ramclustObj$use.findmain[cl]) {2} else {1}  ))
+        font = if(ramclustObj$use.findmain[cl]) {2} else {1}, col = if(ramclustObj$use.findmain[cl]) {2} else {1}  ))
     }
     dev.off()
   }
   
+  #############
+  ## (optionally) write spectra to .mat format in spectra/mat directory
   if(writeMat) {
+    if(!dir.exists("spectra")) {dir.create("spectra")}
+    list.dirs()
     dir.create('spectra/mat')
     for(cl in cmpd) {
-      ms<-ramclustObj$M.ann[[cl]]
-      prcr<-which(ms[,"adduct"] %in% ads)
+      ms<-ramclustObj$M.ann[[cl]] # ms <- matrix(round(rnorm(30, mean = 700, sd = 200), digits=3), ncol = 3)
+      prcr<-which(ms[,"adduct"] %in% ads) # prcr = sample (1:10, 1)
       prcr<-prcr[which.max(ms[prcr,"int"])]
-      prcmz<-ms[prcr,"mz"]
-      prctype<-ms[prcr,"adduct"]
+      prcmz<-ms[prcr,"mz"]  # prcmz <- ms[prcr, 1]
+      prctype<-ms[prcr,"adduct"] # prctype = "[M+H]+"
+      
+      out<-paste(
+        "NAME: ", ramclustObj$cmpd[cl], '\n',
+        "RETENTIONTIME: ", round(ramclustObj$clrt[cl], digits=2), '\n',
+        "PRECURSORMZ: ", prcmz, '\n',
+        "PRECURSORTYPE: ", prctype, '\n',
+        "IONTYPE: ", mode, '\n', 
+        "SPECTRUMTYPE: Centroid", '\n',
+        if((!is.null(ramclustObj$msmsint))) {paste("COLLISIONENERGY: ", as.character(ramclustObj$ExpDes[[2]][which(row.names(ramclustObj$ExpDes[[2]]) == "CE2"),1]), '\n', sep="")},
+        "MSTYPE: ", "MS1", '\n',
+        "Num Peaks: ", nrow(ms), '\n', sep="")
+      for(i in 1:nrow(ms)) {
+        out<-paste(out, ms[i,1], " ", ms[i,2], '\n', sep="")
+      }
+      
       
       if(!is.null(ramclustObj$msmsint)) {
         do<-which(ramclustObj$featclus == cl)
         msms<-cbind(
           'mz' = ramclustObj$fmz[do],
           'int' = ramclustObj$msmsint[do])
-        msms<-msms[which(msms[,"mz"] <= (prcmz + 3)),]
-        msms<-msms[order(msms[,"int"], decreasing=TRUE)]
+        msms<-msms[which(msms[,"mz"] <= (prcmz + 3)),, drop = FALSE]
+        msms<-msms[order(msms[,"int"], decreasing=TRUE),]
+        if(nrow(msms)>0) {
+          out <- paste (out,
+            "MSTYPE:" , "MS2", '\n' ,
+            "Num Peaks: ", nrow(msms), '\n', 
+            sep="")
+          for(i in 1:nrow(msms)) {
+            out<-paste(out, msms[i,1], " ", msms[i,2], '\n', sep="")
+          }
+        }
       }
+      write(out, file=paste0("spectra/mat/", ramclustObj$cmpd[cl], ".mat"))
       
-      sendToMSF(x = ms, 
-                precursormz = prcmz, 
-                precursortype = prctype, 
-                outfile=paste0(getwd(), "/spectra/mat/", ramclustObj$cmpd[cl], ".mat")# ,
-               # MSFexe = "K:/software/MSFinder/MS-FINDER program ver. 2.20"
-                )
+      
+      #   sendToMSF(x = ms, 
+      #             precursormz = prcmz, 
+      #             precursortype = prctype, 
+      #             outfile=paste0(getwd(), "/spectra/mat/", ramclustObj$cmpd[cl], ".mat")# ,
+      #            # MSFexe = "K:/software/MSFinder/MS-FINDER program ver. 2.20"
+      #             )
     }
   }
+  
   cat("finished", '\n')
   return(ramclustObj)
   
 }
 
-# rcbackup<-RC
-# RC2<-do.findmain(ramclustObj = RC, cmpd = c(1:15, 625) )

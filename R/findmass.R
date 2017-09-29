11373598c4c3d4d6ed91c4d49ba920d49b2a3e97
Corey Broeckling <cbroeckl@colostate.edu>
2017-09-29 15:35:13 -0600

    documentation fix

--- R/findmass.R
+++ R/findmass.R
@@ -1,14 +1,16 @@
 #' findmass
 #'
 #' see if any features match a given mass, and whether they are plausibly M0
+#'  @details  a convenience function to perform a targeted search of all feaures for a mass of interest.  Also performs a crude plausibility check as to whether the matched feature could be M0, based on the assumption of approximately 1 carbon per 17 m/z units and natural isottopic abundance of 1.1% 13C
+#' 
 #' @param ramclustObj - the ramclustR object to explore
 #' @param mz - numeric mz value to search for
 #' @param mztol - absolute mass tolerance around mz
-#' @details  a convenience function to perform a targeted search of all feaures for a mass of interest.  Also performs a crude plausibility check as to whether the matched feature could be M0, based on the assumption of approximately 1 carbon per 17 m/z units and natural isottopic abundance of 1.1% 13C
 #' @param rttol - when examining isotope patterns, feaure retention time tolerance around features matching mz +- mztol
 #' @param m.check - logical - check whether the matching masses are plausibly M0.  Looks for ions above and below the target m/z at the same time that have intensities consistent with target ion being a non-M0 isotope.
 #' @return returns a table to the console listing masses which match, their retention time and intensity, and whether it appears to be plausible as M0
-#'#' @export
+#' @keywords 'ramclustR' 'RAMClustR', 'ramclustR'
+#' @export
 
 findmass<-function(
   ramclustObj = RC,

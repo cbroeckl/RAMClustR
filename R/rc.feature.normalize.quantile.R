ebf9b447586d7d153a7d8f44ea6f5f185a1a956a
Zargham Ahmad <zargham.ahmad12@gmail.com>
2023-03-02 14:09:57 +0100

    added msint msmsint computation normalize.quantile

--- R/rc.feature.normalize.quantile.R
+++ R/rc.feature.normalize.quantile.R
@@ -1,3 +1,9 @@
+#' rc.feature.normalize.quantile
+#'
+#' normalize data using quantile
+#'
+#' @param ramclustObj ramclustObj containing MSdata with optional MSMSdata (MSe, DIA, idMSMS)
+#' @return  ramclustR object with normalized data.
 #' @export
 
 rc.feature.normalize.quantile <- function(ramclustObj = NULL) {
@@ -9,6 +15,25 @@ rc.feature.normalize.quantile <- function(ramclustObj = NULL) {
         tmpnames2 <- dimnames(ramclustObj$MSMSdata)
         ramclustObj$MSMSdata <- t(preprocessCore::normalize.quantiles(t(ramclustObj$MSMSdata)))
         dimnames(ramclustObj$MSMSdata) <- tmpnames2
+    }
+
+    ## update msint and optionally msmsint
+    global.min <- apply(cbind(ramclustObj$MSdata, ramclustObj$MSMSdata), 2, "min", na.rm = TRUE)
+
+    ramclustObj$msint <- compute_wt_mean(
+        ramclustObj$MSdata,
+        global.min,
+        ramclustObj$fmz,
+        TRUE
+    )
+
+    if (!is.null(ramclustObj$MSMSdata)) {
+        ramclustObj$msmsint <- compute_wt_mean(
+            ramclustObj$MSMSdata,
+            global.min,
+            ramclustObj$fmz,
+            TRUE
+        )
     }
 
     ramclustObj$history$normalize.quantile <- paste(

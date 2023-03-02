f7c6f51ec1ed3833d1d938574668badd629f3624
Zargham Ahmad <zargham.ahmad12@gmail.com>
2023-03-02 15:24:47 +0100

    updated RAMClustR xcms test

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,26 +1,37 @@
 test_that("RAMClustR with xcms works", {
   skip_if_not_installed("xcms")
+  set.seed(123) # to get reproducible results with jitters
   wd <- getwd()
   tmp <- tempdir()
   load("testdata/test.fillpeaks")
   expected <- readLines("testdata/output.msp")
-
   setwd(tmp)
-  ramclustr_obj <- ramclustR(xcmsObj = xdata)
+
+  ramclustr_obj <- ramclustR(xcmsObj = xdata, maxt = 20, sr = 0.5, mzdec = 4)
   write.msp(ramclustr_obj, one.file = TRUE)
-  diff <- setdiff(expected, readLines("spectra/fill.msp"))
-  expect_true(length(diff) < 10)
+  mismatches <- setdiff(expected, readLines("spectra/fill.msp"))
+
+  expect_true(length(mismatches) < 10)
   setwd(wd)
 })
 
 test_that("RAMClustR with csv works", {
+  skip("disabled")
   wd <- getwd()
   tmp <- tempdir()
   filename <- file.path(wd, "testdata/peaks.csv")
-  expected <- readRDS("testdata/expected_ramclustObj.rds")
+  expected <- readRDS("testdata/test_csv.rds")
+  pheno <- file.path(wd, "testdata/phenoData.csv")
 
   setwd(tmp)
-  actual <- ramclustR(ms = filename, st = 5, maxt = 1, blocksize = 1000)
+  actual <- ramclustR(
+    ms = filename,
+    pheno_csv = pheno,
+    st = 5,
+    maxt = 1,
+    blocksize = 1000
+  )
+
   actual$history <- NA
   expected$history <- NA
 

ac955a4430a7a454acd560662eac50bfe8f0bd52
Zargham Ahmad <zargham.ahmad12@gmail.com>
2023-03-06 13:46:11 +0100

    updated stepwise test

--- tests/testthat/test-stepwise-workflow.R
+++ tests/testthat/test-stepwise-workflow.R
@@ -1,11 +1,12 @@
 test_that("RAMClustR workflow with xcms works", {
   skip_if_not_installed("xcms")
+  set.seed(123) # to get reproducible results with jitters
   wd <- getwd()
   tmp <- tempdir()
   load(file.path("testdata", "test.rc.ramclustr.fillpeaks"))
   expected <- readRDS(file.path("testdata", "do.findmain.rds"))
   setwd(tmp)
-  
+
   ramclustObj <- rc.get.xcms.data(xcmsObj = xdata)
   ramclustObj <- rc.expand.sample.names(ramclustObj = ramclustObj)
   ramclustObj <- rc.feature.replace.na(ramclustObj = ramclustObj)
@@ -16,7 +17,7 @@ test_that("RAMClustR workflow with xcms works", {
   ramclustObj <- rc.qc(ramclustObj = ramclustObj)
   actual <- do.findmain(ramclustObj = ramclustObj)
 
-  expect_equal(actual, expected, tolerance = 0.02)
+  expect_equal(actual, expected)
 
   setwd(wd)
 })
